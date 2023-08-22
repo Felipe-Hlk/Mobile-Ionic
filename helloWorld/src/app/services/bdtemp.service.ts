a0c73783cd7186aa668252cd02e393cb99bcec7e
Felipe Arruda <flphlk@gmail.com>
2023-08-22 20:04:53 -0300

    create service

---
+++ helloWorld/src/app/services/bdtemp.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BdtempService {
+
+  private bd: any[] = [];
+
+  constructor() { }
+
+  salvar(chave: string, valor: any ){
+    
+  }
+}

9475c9eb9d7b0296ff23e9ad815bfaba606ad541
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-08-29 16:55:12 -0300

    adjust components

--- helloWorld/src/app/app.component.ts
+++ helloWorld/src/app/app.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
+interface MenuItem {
+  link: string;
+  icon: string;
+  label: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,5 +12,38 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+
+
+  public smallScreen = false;
+
+  public items_menu: MenuItem[] = [
+    {
+      link: '/home',
+      icon: 'fas fa-chart-line',
+      label: 'Home'
+    },
+    {
+      link: '/produtos',
+      icon: 'fas fa-graduation-cap',
+      label: 'Produtos'
+    },
+    {
+      link: '/carrinho',
+      icon: 'fas fa-headset',
+      label: 'Carrinho'
+    }
+  ];
+
+  public menuName = '';
+  menu: any;
+
+
   constructor() {}
+
+  get sidenavMode() {
+    return this.smallScreen ? 'over' : 'side';
+  }
+
+
+
 }
--- helloWorld/src/app/pages/home/home.page.ts
+++ helloWorld/src/app/pages/home/home.page.ts
@@ -7,6 +7,51 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  produtos = [
+    {
+      nome: 'Produto 1',
+      categoria: 'Eletrônicos',
+      descricao: 'Descrição do Produto 1.',
+      imagem: 'caminho/para/imagem1.jpg'
+    },
+    {
+      nome: 'Produto 2',
+      categoria: 'Vestuário',
+      descricao: 'Descrição do Produto 2.',
+      imagem: 'caminho/para/imagem2.jpg'
+    },
+    {
+      nome: 'Produto 3',
+      categoria: 'Esporte',
+      descricao: 'Descrição do Produto 2.',
+      imagem: 'caminho/para/imagem2.jpg'
+    },
+    {
+      nome: 'Produto 4',
+      categoria: 'Eletrodoméstico',
+      descricao: 'Descrição do Produto 2.',
+      imagem: 'caminho/para/imagem2.jpg'
+    },
+    {
+      nome: 'Produto 5',
+      categoria: 'Eletrodoméstico',
+      descricao: 'Descrição do Produto 2.',
+      imagem: 'caminho/para/imagem2.jpg'
+    },
+    {
+      nome: 'Produto 6',
+      categoria: 'Eletrodoméstico',
+      descricao: 'Descrição do Produto 2.',
+      imagem: 'caminho/para/imagem2.jpg'
+    },
+    {
+      nome: 'Produto 7',
+      categoria: 'Eletrodoméstico',
+      descricao: 'Descrição do Produto 2.',
+      imagem: 'caminho/para/imagem2.jpg'
+    },
+  ];
+
   constructor() {}
 
 }

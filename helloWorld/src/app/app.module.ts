18fb622a91e4cab73dd92379e1a66d3d059cca99
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-08-30 17:21:04 -0300

    Update app.module.ts

--- helloWorld/src/app/app.module.ts
+++ helloWorld/src/app/app.module.ts
@@ -6,10 +6,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { NavbarPage } from './shared/navbar/navbar.page';
+
 
 @NgModule({
-  declarations: [AppComponent, NavbarPage],
+  declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
